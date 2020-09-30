86da024804eec2b3e2ecec65bd4d72f161120fb2
acrux95 <67486473+ArmandoCloud@users.noreply.github.com>
2020-09-29 23:11:11 -0500

    Finishing react basic project

--- src/components/HelloWorld.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../assets/styles/App.scss'
-
-const helloWorld = () => (
-    <h1>Hola Mundo</h1>
-)
-
-export default helloWorld;
--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,18 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ]
+            },
+            {
+            
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'assets/[hash].[ext]'
+                    }
+                  },
+                ]
             }
         ]
     },
